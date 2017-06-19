55e99ab57bab51d435295c3beaed870a356c541e
GMRDL1 <gmrstudent@outlook.com>
2017-06-19 16:09:24 -0700

    initial commit

---
+++ Node.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkList
+{
+    class Node<T>
+    {
+        public T Item;
+        public Node<T> NextNode;
+        public Node(T item, Node<T> next = null)
+        {
+            Item = item;
+            NextNode = next;
+        }
+        public Node<T> AddAfter(T item)
+        {
+            Node<T> newNode = new Node<T>(item);
+            NextNode.Item = item;
+            return newNode;
+        }
+        public Node<T> RemoveAfter()
+        {
+            Node<T> nextNode = NextNode.NextNode;
+            NextNode = NextNode.NextNode;
+            return nextNode;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                DavidLinkedListt<int> linklist = new DavidLinkedListt<int>;
+                Console.WriteLine("_____________________________");
+                Console.WriteLine("Operation: ");
+                string operation = Console.ReadLine();
+                Console.WriteLine("Arguements: ");
+                string arguements = Console.ReadLine();
+
+                if(operation.ToLower() == "addtofront")
+                {
+                }
+                else if (operation.ToLower() == "addtoend")
+                {
+
+                }
+                else if (operation.ToLower() == "removefromfront")
+                {
+
+                }
+                else if (operation.ToLower() == "removefromend")
+                {
+
+                }
+            }
+        }
+    }
+}
